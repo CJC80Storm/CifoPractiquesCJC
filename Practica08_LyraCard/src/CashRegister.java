cce391a7fe92e397d00b8621c51dfddb84f2c5f2
mati <mati@DESKTOP-5UOP2KP>
2020-10-16 13:57:48 +0200

    V0

---
+++ Practica08_LyraCard/src/CashRegister.java
@@ -0,0 +1,16 @@
+
+public class CashRegister {
+	private double cashInRegister;
+	 private int economicalSold;
+	 private int gourmetSold;
+	 
+	 public CashRegister() {
+		 double cashInRegister=1000;
+		 System.out.println("Start of shaft. The balance is: "+cashInRegister);
+	 }
+	 public double payEconomical(double cashGiven) {
+		 
+		 
+		 return 0.0;
+	 }
+}
