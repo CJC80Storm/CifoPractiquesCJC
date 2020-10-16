29e0cc12fc4f13564b32ea715b09ff8eeadf84fa
mati <mati@DESKTOP-5UOP2KP>
2020-10-16 12:35:38 +0200

    V0

---
+++ Practica07_flats/src/Aparment.java
@@ -0,0 +1,14 @@
+
+public class Aparment {
+	private int rooms; 
+	private int squareMeters; 
+	private int pricePerSquareMeter;
+	
+	public Aparment(int rooms, int squareMeters, int pricePerSquareMeter){ 
+			this.rooms = rooms; 
+			this.squareMeters = squareMeters; 
+			this.pricePerSquareMeter = pricePerSquareMeter;
+	}
+	
+
+}
---
+++ Practica07_flats/src/Properties.java
@@ -0,0 +1,13 @@
+
+public class Properties {
+	
+	public static void main (String[] args) {
+		Aparment studioManhattan = new Aparment(1, 16, 5500);
+		Aparment twoRoomsBrooklyn = new Aparment(2, 38, 4200); 
+		Aparment fourAndKitchenBronx = new Aparment(3, 78, 2500); 
+		
+		
+		
+	}
+
+}
