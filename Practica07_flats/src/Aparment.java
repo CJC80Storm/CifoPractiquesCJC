3dec7ed98432f8a8f11ebd7f29365b415aa8ba19
mati <mati@DESKTOP-5UOP2KP>
2020-10-16 13:28:53 +0200

    Versió final per a Albert.

--- Practica07_flats/src/Aparment.java
+++ Practica07_flats/src/Aparment.java
@@ -9,6 +9,33 @@ public class Aparment {
 			this.squareMeters = squareMeters; 
 			this.pricePerSquareMeter = pricePerSquareMeter;
 	}
-	
+	public int priceDifference(Aparment otherAparment) {
+		int a=this.squareMeters*this.pricePerSquareMeter;
+		int b=otherAparment.pricePerSquareMeter*otherAparment.squareMeters;
+		return Math.abs(a-b);
+	}
+	 public boolean moreExpensiveThan (Aparment otherAparment) {
+		int priceApartmemt1=this.pricePerSquareMeter*this.squareMeters;
+		//System.out.println(priceApartmemt1);
+		int priceApartment2=otherAparment.pricePerSquareMeter*otherAparment.squareMeters;
+		//System.out.println(priceApartment2);
+		if (priceApartmemt1>priceApartment2) {
+			return true;
+		}else {
+			return false;
+		}
+	 }
+	 
+	 public boolean larger(Aparment otherAparment) {
+		 int Apart1=this.squareMeters;
+		 int Apart2=otherAparment.squareMeters;
+		 if(Apart1>Apart2) {
+			 return true;
+		 } else {
+			 return false;
+		 }
+		
+		
+	 }
 
 }
--- Practica07_flats/src/Properties.java
+++ Practica07_flats/src/Properties.java
@@ -6,8 +6,14 @@ public class Properties {
 		Aparment twoRoomsBrooklyn = new Aparment(2, 38, 4200); 
 		Aparment fourAndKitchenBronx = new Aparment(3, 78, 2500); 
 		
-		
-		
+		System.out.println( studioManhattan.larger(twoRoomsBrooklyn) );
+		System.out.println( fourAndKitchenBronx.larger(twoRoomsBrooklyn) );
+		System.out.println( studioManhattan.priceDifference(twoRoomsBrooklyn) );
+		System.out.println( fourAndKitchenBronx.priceDifference(twoRoomsBrooklyn) ); 
+		System.out.println( studioManhattan.moreExpensiveThan(twoRoomsBrooklyn) );
+		System.out.println( fourAndKitchenBronx.moreExpensiveThan(twoRoomsBrooklyn) ); 
+	
+	
 	}
-
+	
 }
