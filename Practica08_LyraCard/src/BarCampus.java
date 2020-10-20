4ed15f0ce3ede73034b5ed24c0dff61feb33d576
mati <mati@DESKTOP-5UOP2KP>
2020-10-20 09:44:55 +0200

    V1

---
+++ Practica08_LyraCard/src/BarCampus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BarCampus {
+	public static void main(String[] args) {
+		double cash;
+		double cashGroceries;
+
+		System.out.println("Introduce balance: (in €, Euros)");
+		Scanner money= new Scanner(System.in);
+		cash= money.nextDouble();
+		LyyraCard cardPerson = new LyyraCard (cash);
+		//CashRegister register1 = new CashRegister ((CashRegister).balance);
+		//CashRegister.CashRegister();
+
+
+
+		}
+
+}
--- Practica08_LyraCard/src/LyyraCard.java
+++ Practica08_LyraCard/src/LyyraCard.java
@@ -1,22 +1,29 @@
+import java.util.Scanner;
 
 public class LyyraCard {
-	private double balance;
-	    public LyyraCard(double balance) {
-	        this.balance = balance;
+	public double balance;
+
+	public LyyraCard(double balance) {
+	Scanner amountcard = new Scanner(System.in);
+	this.balance= amountcard.nextDouble();
 	    }
 
-	    public double balance() {
+	    public double totalBalance() {
 	        return this.balance;
 	    }
 
 	    public void loadMoney(double amount) {
-	        this.balance += amount;
+	        double b=this.balance;
+	        this.balance= b-amount;
 	    }
 
 	    public boolean pay(double amount){
 	       // the method checks if the balance of the card is at least the amount given as parameter if not, the method returns false meaning that the card could not be used for the payment if the balance is enough, the given amount is taken from the balance and true is returned
 	    double a=amount;
+	    if(this.balance>=a){
 	    return true;
+	    }else {
+	    return false;
 	    }
-	 
+	    }
 }
