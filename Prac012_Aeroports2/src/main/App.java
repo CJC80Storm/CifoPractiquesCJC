0531a70d14af0771b6ae3c1427a05fa02f5fa7a7
CJC <cjove.java@gmail.com>
2020-11-17 10:43:35 +0100

    versió final

---
+++ Prac012_Aeroports2/src/main/App.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+//import java.util.Scanner;
+
+import aeroprts.Plane;
+import menu.Consola;
+import menu.MenuFirstOptions;
+
+
+public class App {
+	
+	public static void main (String[] args) {
+		boolean controlOptions=true;
+		boolean controlPrint=true;
+		Scanner reader=new Scanner(System.in);		
+		ArrayList<Plane> planes= new ArrayList<Plane>();
+		
+		while (controlOptions) {
+			controlOptions=MenuFirstOptions.menuConsola(planes, reader);
+		}
+		
+		while (controlPrint) {
+			controlPrint=Consola.menuPrint(planes,reader);
+		}
+		
+		System.out.println("Thanks for using the service of Airports. See you in sky!!");
+	}
+
+}
