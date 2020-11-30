34c4fdf450b9338d386ba8b81ecdfeac0e444717
CJC <cjove.java@gmail.com>
2020-11-30 12:57:31 +0100

    versió funcional

---
+++ 008Part012HashForDate/src/main/AppTest.java
@@ -0,0 +1,15 @@
+package main;
+
+import dates.SimpleDate;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		SimpleDate d = new SimpleDate(1, 2, 2000);
+		System.out.println(d.equals("heh"));
+		System.out.println(d.equals(new SimpleDate(5, 2, 2012)));
+		SimpleDate e = new SimpleDate(1, 2, 2000);
+		System.out.println(d.equals(e));
+		System.out.println(d.hashCode());
+	}
+}
